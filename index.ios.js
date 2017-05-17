34b3898791fd213f4b42b419e7f4985cc5ad1229
Igor <t051200@yandex.ru>
2017-05-17 23:05:28 +0300

    add empty ios

---
+++ index.ios.js
@@ -0,0 +1,36 @@
+var application = require("application");
+
+module.exports = 
+{
+    initialize: function(context) 
+    {
+    },
+
+    login: function(scope) 
+    {
+    },
+
+    logout: function() 
+    {
+    },
+
+    isLoggedIn: function() 
+    {
+        return null;
+    },
+
+    getApiVersion: function() 
+    {
+        return null;
+    },
+
+    getAccessToken: function() 
+    {
+        return null;
+    },
+
+    onActivityResult: function(requestCode, resultCode, data, onResult, onError) 
+    {
+        return null;
+    },
+};
